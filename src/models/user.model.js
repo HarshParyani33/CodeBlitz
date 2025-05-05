e7f9ceb222048c011d8be19660a2adc4966ec79d
Harsh Paryani <harshparyani68@gmail.com>
2025-05-05 20:35:11 +0530

    Add user schema with fields for username, password, email, avatar, bio, game history, rating, and GitHub

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,5 +1,43 @@
 import mongoose from "mongoose";
 
-const userSchema = new mongoose.Schema({},{timestamps:true});
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    password: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    email: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+    },
+    avatar: {
+        type: String,
+    },
+    bio: {
+        type: String,
+    },
+    gameHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Game",
+        }
+    ],
+    rating: {
+        type: Number,
+        default: 0,
+    },
+    github: {
+        type: String,
+    },
+
+},{timestamps:true});
 
 const User = mongoose.model("User", userSchema);
